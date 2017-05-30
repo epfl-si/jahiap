a2c835557ee848813392b3b32b03ee3ed5cdfdf5
GregLeBarbar <gregory.charmier.epfl.ch>
2017-05-30 10:47:46 +0200

    Add copyrights on python files

--- exporter.py
+++ exporter.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python
 
+"""(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017"""
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 import os
--- jahiap.py
+++ jahiap.py
@@ -1,3 +1,4 @@
+"""(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017"""
 from slugify import slugify
 
 import xml.dom.minidom
