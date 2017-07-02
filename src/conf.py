71712329ce904381d983728bb458b8974dbd7d85
ebreton <manu@ibimus.com>
2017-07-02 19:22:42 +0200

    ignore submodule jahia-data

--- src/conf.py
+++ src/conf.py
@@ -80,4 +80,4 @@ author = u'epfl-idevelop'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # These patterns also affect html_static_path and html_extra_path
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', 'jahiap-data']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store', 'jahia-data']
